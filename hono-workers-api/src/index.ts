891a98b74b8ebd4de86a402ef62be6869f5a9f3b
devfxx <54813298+devfxx@users.noreply.github.com>
2025-01-02 18:54:24 +0200

    Add download endpoint to fetch video clips and handle URL validation

--- hono-workers-api/src/index.ts
+++ hono-workers-api/src/index.ts
@@ -29,6 +29,48 @@ app.post("/api/clip", async (c) => {
   return await handleClipRequest(url, c);
 });
 
+app.get("/api/download", async (c) => {
+  const url = c.req.query("url");
+  if (!url) {
+    return c.json<ClipResponse>(
+      {
+        valid: false,
+        reasoning: "No URL provided",
+      },
+      400
+    );
+  }
+
+  const mUrl = configureURL(url);
+
+  if (!mUrl || !checkURL(mUrl)) {
+    return c.json<ClipResponse>(
+      {
+        valid: false,
+        reasoning: "Invalid URL",
+      },
+      400
+    );
+  }
+
+  const videoURL = await getVideoURL(mUrl);
+  if (!videoURL) {
+    return c.json<ClipResponse>(
+      {
+        valid: false,
+        reasoning: "Error fetching video",
+      },
+      500
+    );
+  }
+
+  const res = await fetch(videoURL);
+
+  c.header("Content-Type", "video/mp4");
+  c.header("Content-Disposition", `attachment; filename="clip.mp4"`);
+  return c.newResponse(res.body);
+});
+
 // Catch-all redirect
 app.get("*", (c) => {
   return c.redirect(
@@ -52,16 +94,17 @@ async function handleClipRequest(inputtedUrl: string, c: Context) {
 
   try {
     const src = await getVideoURL(url);
-    if (src) {
-      return c.json<ClipResponse>({ valid: true, src });
+    if (!src) {
+      return c.json<ClipResponse>(
+        {
+          valid: false,
+          reasoning: "No clip found",
+        },
+        404
+      );
     }
-    return c.json<ClipResponse>(
-      {
-        valid: false,
-        reasoning: "No clip found",
-      },
-      404
-    );
+
+    return c.json<ClipResponse>({ valid: true, src });
   } catch (error) {
     return c.json<ClipResponse>(
       {
