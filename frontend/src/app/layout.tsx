64afb0c94cf79d2ebc0163e2e905d5f68d06484c
devfxx <54813298+devfxx@users.noreply.github.com>
2025-01-03 01:59:10 +0200

    Update layout metadata and viewport settings for Medal Bypass

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,37 @@
 import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import type { Viewport, Metadata } from "next";
+import _static from "@/static";
 
 export const metadata: Metadata = {
-  title: "Medal Downloader",
-  description: "Made with 💜",
+  title: "Medal Bypass | xvht",
+  description:
+    "Download Medal clips without the app. No ads, no tracking, no bullshit.",
+  openGraph: {
+    type: "website",
+    url: "https://md.xvh.lol",
+    title: "Medal Bypass | xvht",
+    description:
+      "Download Medal clips without the app. No ads, no tracking, no bullshit.",
+  },
+  creator: "xvht",
+  keywords: [
+    "medal",
+    "medal.tv",
+    "medal bypass",
+    "medal downloader",
+    "medal clip downloader",
+    "medal clip",
+    "medal clip bypass",
+    "medal clip download",
+  ],
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  themeColor: _static.colors.first,
 };
 
 export default function RootLayout({
